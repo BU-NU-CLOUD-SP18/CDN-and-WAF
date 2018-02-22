92585501df38ca194260d56f0970999dc7d98db5
Ubuntu <ubuntu@massopen.cloud>
2018-02-22 21:59:16 +0000

    push signup page

--- web/FlaskServer.py
+++ web/FlaskServer.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Welcome to 'CDN with WAF' project!</h1>"
+
+@application.route("/signup")
+def signUp():
+    return render_template('register.html')
+
+@application.route("/login")
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True, port=6081)
