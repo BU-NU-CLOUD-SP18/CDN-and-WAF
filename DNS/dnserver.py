6a2077e28cf1d2c957fe9c96249932d658722b0b
Anand Sanmukhani <anandsanmukhani@me.com>
2018-03-18 14:26:56 -0400

    return just one A IP address

--- DNS/dnserver.py
+++ DNS/dnserver.py
@@ -4,17 +4,21 @@ import socket
 from dnslib import *
 
 port = 53
-ip = '127.0.0.1'
+ip = '0.0.0.0'
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((ip,port))
 
 while 1:
     data, addr = sock.recvfrom(512)
-    print(data)
+    # print("This is data:")
+    # print(data)
     question = DNSRecord.parse(data)
+    parsedQ = question.get_q()
+    print("This is the Parsed Question")
     print(question)
     answer = question.reply()
-    answer.add_answer(*RR.fromZone("abc.com 60 CNAME www.google.com"))
+    answer.add_answer(*RR.fromZone(str(parsedQ.get_qname())+" 299 A 172.217.12.196"))
+    # print("This is the response packet")
     # print(answer)
     sock.sendto(answer.pack(), addr)
