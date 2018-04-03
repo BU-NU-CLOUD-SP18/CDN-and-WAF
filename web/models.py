655436281b45508efd40d98c68fe34a7e102a140
EdwardXu <edwardxu@bu.edu>
2018-04-03 15:11:20 -0400

    update web server

--- web/models.py
+++ web/models.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
-# engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
+# engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
+engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
 Base = declarative_base()
 Base.metadata.reflect(engine)
 
@@ -16,24 +16,31 @@ from sqlalchemy.orm import relationship, backref
 # database model from Postgres cdn. Table name 'UserData'
 class Users(Base):
     __table__ = Base.metadata.tables['UserData']
-    def __init__(self, username, email, passwd, originip):
+    def __init__(self, username, email, passwd):
         self.username = username
         self.email = email
         self.passwd = passwd
-        self.originip = originip
 
 class Instances(Base):
     __table__ = Base.metadata.tables['InstanceData']
-    def __init__(self, cacheip, cachename, cpu, storage):
+    def __init__(self, cacheip, remark, cpu, storage):
         self.cacheip = cacheip
-        self.cachename = cachename
+        self.remark = remark
         self.cpu = cpu
         self.storage = storage
 
 class Joins(Base):
     __table__ = Base.metadata.tables['CacheMatch']
-    def __init__(self, originip, cacheip):
-        self.originip = originip
+    def __init__(self, email, hostname, cacheip, origin_hostname):
+        self.email = email
+        self.hostname = hostname
+        self.cacheip = cacheip
+        self.origin_hostname = origin_hostname
+
+class CNAME(Base):
+    __table__ = Base.metadata.tables['CNAMEmatch']
+    def __init__(self, cname, cacheip):
+        self.cname = cname
         self.cacheip = cacheip
 
 """
