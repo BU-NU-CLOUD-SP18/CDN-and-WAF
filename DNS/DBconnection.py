a7e5104eedc3379b3b6916d9deed566d3995f512
Ubuntu <ubuntu@DNS.massopen.cloud>
2018-04-05 03:24:46 +0000

    pushong some changes to prevent merge conflicts

--- DNS/DBconnection.py
+++ DNS/DBconnection.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models import Users
 
-# engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
-engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
+engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
+# engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
 
 Session = sessionmaker(bind=engine)
-session = Session()
-for instance in session.query('users'):
+session = scoped_session(sessionmaker(bind=engine))
+for instance in session.query(Users.email).all():
     print(instance)
 # print(session.query('users'))
---
+++ DNS/models.py
@@ -0,0 +1,73 @@
+# models.py
+#import flask
+#from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
+# engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
+Base = declarative_base()
+Base.metadata.reflect(engine)
+
+from sqlalchemy.orm import relationship, backref
+
+
+
+# database model from Postgres cdn. Table name 'UserData'
+class Users(Base):
+    __table__ = Base.metadata.tables['UserData']
+    def __init__(self, username, email, passwd, originip):
+        self.username = username
+        self.email = email
+        self.passwd = passwd
+        self.originip = originip
+
+class Instances(Base):
+    __table__ = Base.metadata.tables['InstanceData']
+    def __init__(self, cacheip, cachename, cpu, storage):
+        self.cacheip = cacheip
+        self.cachename = cachename
+        self.cpu = cpu
+        self.storage = storage
+
+class Joins(Base):
+    __table__ = Base.metadata.tables['CacheMatch']
+    def __init__(self, originip, cacheip):
+        self.originip = originip
+        self.cacheip = cacheip
+
+"""
+db = SQLAlchemy()
+
+# database model
+class User(db.Model):
+    __tablename__ = "users"
+    name = db.Column(db.String(120), primary_key=True, unique=True)
+    email = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    originip = db.Column(db.String(120))
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.originip = None
+
+    def __repr__(self):
+        return '<E-mail %r>' % self.email
+
+class Instance(db.Model):
+    __tablename__ = "instances"
+    varnishIp = db.Column(db.Integer, primary_key=True)
+    cpu = db.Column(db.String(120), unique=True)
+    status = db.Column(db.String(120))
+
+    def __init__(self, varnishIp, cpu, status):
+        self.varnishIp = varnishIp
+        self.cpu = cpu
+        self.status = status
+
+    def __repr__(self):
+        return '<varnishIp %r, >' % self.varnishIp"""
+
+
