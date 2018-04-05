6c2853269e66dc3fd496adf643192b635e89c33f
Anand Sanmukhani <anandsanmukhani@me.com>
2018-04-04 23:54:59 -0400

    fixed merge conflicts and testing DBconnection

--- DNS/DBconnection.py
+++ DNS/DBconnection.py
@@ -1,29 +1,28 @@
-<<<<<<< HEAD
 from sqlalchemy import create_engine
+
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models import Users
 
-engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
-# engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
+# engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
+engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
 
-Session = sessionmaker(bind=engine)
-session = scoped_session(sessionmaker(bind=engine))
-for instance in session.query(Users.email).all():
-    print(instance)
-=======
-import flask
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import scoped_session, sessionmaker, Query
-
-from models import *
+# Session = sessionmaker(bind=engine)
+# session = scoped_session(sessionmaker(bind=engine))
+# for instance in session.query(Users.email).all():
+#     print(instance)
+# =======
+# import flask
+# from flask import Flask, render_template, request, redirect, url_for, flash
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import scoped_session, sessionmaker, Query
+#
+# from models import *
 
 db_session = scoped_session(sessionmaker(bind=engine))
-Users = db_session.query(Users.email).all()
+Users = db_session.query(Users.username).all()[0]
 
 for User in Users:
-    print(User.email)
+    print(User)
 
 
->>>>>>> 16c033f515491bb80b78a9d0afb214ecb1561908
 # print(session.query('users'))
--- DNS/models.py
+++ DNS/models.py
@@ -1,13 +1,4 @@
 # models.py
-<<<<<<< HEAD
-#import flask
-#from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
-# engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
-=======
 import flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
@@ -15,7 +6,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # engine = create_engine('postgresql://ubuntu:528@10.0.0.9/cdn')
 engine = create_engine('postgresql://ubuntu:528@128.31.25.73/cdn')
->>>>>>> 16c033f515491bb80b78a9d0afb214ecb1561908
 Base = declarative_base()
 Base.metadata.reflect(engine)
 
@@ -26,19 +16,6 @@ from sqlalchemy.orm import relationship, backref
 # database model from Postgres cdn. Table name 'UserData'
 class Users(Base):
     __table__ = Base.metadata.tables['UserData']
-<<<<<<< HEAD
-    def __init__(self, username, email, passwd, originip):
-        self.username = username
-        self.email = email
-        self.passwd = passwd
-        self.originip = originip
-
-class Instances(Base):
-    __table__ = Base.metadata.tables['InstanceData']
-    def __init__(self, cacheip, cachename, cpu, storage):
-        self.cacheip = cacheip
-        self.cachename = cachename
-=======
     def __init__(self, username, email, passwd):
         self.username = username
         self.email = email
@@ -49,53 +26,11 @@ class Instances(Base):
     def __init__(self, cacheip, remark, cpu, storage):
         self.cacheip = cacheip
         self.remark = remark
->>>>>>> 16c033f515491bb80b78a9d0afb214ecb1561908
         self.cpu = cpu
         self.storage = storage
 
 class Joins(Base):
     __table__ = Base.metadata.tables['CacheMatch']
-<<<<<<< HEAD
-    def __init__(self, originip, cacheip):
-        self.originip = originip
-        self.cacheip = cacheip
-
-"""
-db = SQLAlchemy()
-
-# database model
-class User(db.Model):
-    __tablename__ = "users"
-    name = db.Column(db.String(120), primary_key=True, unique=True)
-    email = db.Column(db.String(120))
-    password = db.Column(db.String(120))
-    originip = db.Column(db.String(120))
-
-    def __init__(self, name, email, password):
-        self.name = name
-        self.email = email
-        self.password = password
-        self.originip = None
-
-    def __repr__(self):
-        return '<E-mail %r>' % self.email
-
-class Instance(db.Model):
-    __tablename__ = "instances"
-    varnishIp = db.Column(db.Integer, primary_key=True)
-    cpu = db.Column(db.String(120), unique=True)
-    status = db.Column(db.String(120))
-
-    def __init__(self, varnishIp, cpu, status):
-        self.varnishIp = varnishIp
-        self.cpu = cpu
-        self.status = status
-
-    def __repr__(self):
-        return '<varnishIp %r, >' % self.varnishIp"""
-
-
-=======
     def __init__(self, email, hostname, cacheip, origin_hostname):
         self.email = email
         self.hostname = hostname
@@ -107,4 +42,3 @@ class CNAME(Base):
     def __init__(self, cname, cacheip):
         self.cname = cname
         self.cacheip = cacheip
->>>>>>> 16c033f515491bb80b78a9d0afb214ecb1561908
